7708a5b29e3a05ccbf6aa18d56f76d2c9a25fc5e
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-11-04 18:38:14 +0000

    feature: add models for goals, tasks and scheduling

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Goal, Task, Schedule, ScheduledDate
 
-# Register your models here.
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at',
+                    'expected_deadline', 'status')
+    list_filter = ('status', 'created_at', 'updated_at')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'due_date', 'completed')
+    list_filter = ('created_at', 'due_date', 'completed')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Schedule)
+admin.site.register(ScheduledDate)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Goal(models.Model):
+    STATUS = [
+        ('0', "ToDo"),
+        ('1', "In Progress"),
+        ('2', "On Hold"),
+        ('3', "Done"),
+    ]
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    expected_deadline = models.DateField()
+    status = models.CharField(max_length=1, choices=STATUS)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Schedule(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    scheduled_dates = models.ManyToManyField('ScheduledDate')
+
+    def num_of_scheduled_dates(self):
+        return self.scheduled_dates.count()
+
+    def __str__(self):
+        return self.task.title
+
+
+class ScheduledDate(models.Model):
+    date = models.DateField()
+    scheduled_time = models.TimeField()
+
+    class Meta:
+        unique_together = ['date', 'scheduled_time']
+
+    def __str__(self):
+        return f"{self.date} {self.scheduled_time}"
