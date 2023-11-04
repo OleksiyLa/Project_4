9795173d2ccb24047ca0879e03f8cda906f16018
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-11-04 13:54:56 +0000

    deployment

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['task-manager-planner-app.herokuapp.com']
+ALLOWED_HOSTS = ['task-manager-planner-app-ca416dc67970.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
