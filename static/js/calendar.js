b2dde21e912b194cd6648a50a71fdf4748106c74
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-11-22 13:39:39 +0000

    change scheduled tasks in calendar by removing description and done message, postion title above the time

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -169,25 +169,22 @@ function getTasksHTML(date){
     for(let i = 0; i < schedule.length; i++) {
       const completed = schedule[i].completed;
       const classCompleted = completed ? "completed" : "not-completed";
-      const isDone = completed ? "Done" : "Not done";
       const deleteIcon = completed ? "" : `<img src="/static/svg/delete_icon.svg" alt="delete-icon" class="float-end delete-icon control-icon" data-txt="scheduled task" data-url="/delete_scheduled_task/${schedule[i].slug}">`;
       const editIcon = `<a class="my-1" href="/edit_scheduled_task/${schedule[i].slug}">
       <img src="/static/svg/edit_icon.svg" alt="edit-icon" class="control-icon"></a>`;
       const finishTaskBtn = completed ? "" : `<a class="btn btn-outline-dark w-100 my-2" href="/complete_scheduled_task/${schedule[i].slug}">
       <strong>Complete</strong></a>`;
-      const timeDone = completed ? "" : `<h3 class="h4 text-center my-2">From <span>${schedule[i].start_time.slice(0, -3)}</span> To <span>${schedule[i].end_time.slice(0, -3)}</span></h3>`;
+      const timeDone = `<h3 class="h4 text-center my-2">From <span>${schedule[i].start_time.slice(0, -3)}</span> To <span>${schedule[i].end_time.slice(0, -3)}</span></h3>`;
 
       tasksArrHTML.push([schedule[i].start_time, `
-      <div class="card mb-3 bg-light ${classCompleted}">
-        <h2 class="text-center">${isDone}</h2>
+      <div class="card p-2 mb-3 bg-light ${classCompleted}">
+        <h2 class="text-center">${item.title}</h2>
         <div class="card-body p-3 ${classCompleted}">
           <div class="float-end d-flex flex-column">
             ${deleteIcon}
             ${editIcon}
           </div>
           ${timeDone}
-          <h3 class="h4 text-center my-2">${item.title}</h3>
-          <p class="text-center">${item.description}</p>
           <div class="mt-3">
             ${finishTaskBtn}
           </div>
